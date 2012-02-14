43788deeb51d799261249ae9717f8c7b160c6851
Matthew Jeffryes <matt@mjeffryes.net>
2012-02-14 06:08:38 -0800

    factor out common tagging code

--- resources/exploration_misc.rb
+++ resources/exploration_misc.rb
@@ -69,51 +69,50 @@ def fetch_from_linked_in file='1201285931_results.tsv', outfile='local_filtered_
   puts "DONE"
 end
 
-def tag_linkedin
+def tag( query, tag )
 	db = Mongo::Connection.new.db('crunch_data_new')
 	people = db.collection("person")
-	people.find({ "$and" => [ {"crunch_profile.0.web_presences.0" => {"$exists"=> "true"}}, {"linkedin_url" => {"$exists" => 0}} ] }).each do |person|
-		linkedin_url = nil
+	people.find( { "$and" => [ query, { tag => { "$exists" => 0 } } ] } ).each do |person|
+		person[tag] = yield( person )
+		people.save(person) if person[tag] 
+	end
+end
+
+def tag_twitter
+	tag( { "crunch_profile.0.web_presences.0" => { "$exists"=> "true" } }, "twitter_url" ) do |person|
 		person["crunch_profile"][0]["web_presences"].each do |hash|
-			linkedin_url = hash["external_url"] if hash["external_url"] =~ /linkedin\.com\/in/
+			next hash["external_url"] if hash["external_url"] =~ /twitter\.com\/in/
 		end if person["crunch_profile"][0]["web_presences"]
-		if linkedin_url then
-			person["linkedin_url"] = linkedin_url
-			people.save(person)
-		end
+	end
+end
+
+def tag_linkedin
+	tag( { "crunch_profile.0.web_presences.0" => { "$exists"=> "true" } }, "linkedin_url" ) do |person|
+		person["crunch_profile"][0]["web_presences"].each do |hash|
+			next hash["external_url"] if hash["external_url"] =~ /linkedin\.com\/in/
+		end if person["crunch_profile"][0]["web_presences"]
 	end
 end
 
 def tag_locality
-    require 'mechanize'
-    agent = Mechanize.new
-    agent.history.max_size = 10
-	db = Mongo::Connection.new.db('crunch_data_new')
-	people = db.collection("person")
-	people.find({ "$and" => [ { "linkedin_url" => {"$exists" => 1} }, { "locality" => {"$exists" => 0 } } ] }).each do |person|
-	  begin
-        page = agent.get person["linkedin_url"]
-      rescue Exception => e
-        p e.inspect
-		sleep 0.25 #don't want to get banned
-      end
-      if page then
-        person["locality"] = page.search('//span[@class="locality"]').inner_text.strip!
-        people.save(person)
-      end
+	require 'mechanize'
+	agent = Mechanize.new
+	agent.history.max_size = 10
+	tag( { "linkedin_url" => { "$exists" => 1 } }, "locality" ) do |person|
+		begin
+			page = agent.get person["linkedin_url"]
+		rescue Exception => e
+			p e.inspect
+			sleep 0.25 #don't want to get banned
+		end
+		next page.search('//span[@class="locality"]').inner_text.strip! if page
 	end
 end
 
 def tag_exec regex = /(C.O|Advisor|Director|Chairman|Marketing|Board Member|President|VP, Sales|Vice President, Sales|Chief (Executive|Operations|Operating|Revenue) Officer|Founder|Managing Partner|SVP|VP Product)/i
-	db = Mongo::Connection.new.db('crunch_data_new')
-	people = db.collection("person")
-	people.find({}).each do |p|
+	tag( {}, "is_exec" ) do |p|
   		p["relationships"].each do |r|
-    	    if r["title"] =~ regex then
-    	    {
-    	    	p["is_exec"] = 1
-    	    	people.save(p)
-    	    }
+    	    next 1 if r["title"] =~ regex 
     	end if p["relationships"]
 	end
 end
